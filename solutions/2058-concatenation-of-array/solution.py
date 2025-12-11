5e534860e43a7f14abcb3d450a24d75ac6e8a571
Sudharshan.R <sudhir.ssr7@gmail.com>
2025-12-11 07:43:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- solutions/2058-concatenation-of-array/solution.py
+++ solutions/2058-concatenation-of-array/solution.py
@@ -1,7 +1,4 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        n=[]
-        for i in range(2):
-            for j in range(len(nums)):
-                n.append(nums[j])
-        return n
+        ans=nums+nums
+        return ans
