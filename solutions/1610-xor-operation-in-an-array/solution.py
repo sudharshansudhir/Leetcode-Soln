71e162f3e432a002886512922c15654004fd6dd3
Sudharshan.R <sudhir.ssr7@gmail.com>
2025-12-22 05:58:07 +0000

    Sync LeetCode submission Runtime - 4 ms (1.81%), Memory - 17.2 MB (100.00%)

---
+++ solutions/1610-xor-operation-in-an-array/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums=[]
+        for i in range(n):
+            nums.append(start+2*i)
+        ans=0
+        for i in nums:
+            ans=ans^i
+        return ans
