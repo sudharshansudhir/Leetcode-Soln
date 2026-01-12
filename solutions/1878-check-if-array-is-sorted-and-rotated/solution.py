8c5215adba057843806512ecf9e600c989b0f9d5
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-01-12 10:33:26 +0000

    Sync LeetCode submission Runtime - 6 ms (2.47%), Memory - 19.3 MB (16.58%)

---
+++ solutions/1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n=len(nums)
+        t=nums[:]
+        t.sort()
+        flag=False
+        for i in range(n):
+            s=t[n-i:]+t[:n-i]
+            print(s)
+            if(s==nums):
+                flag=True
+                break
+        return flag
