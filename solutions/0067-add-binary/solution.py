d1e2a26a26c8ceb9f96533cee9e26f2f94389e45
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-02-15 09:06:23 +0000

    Sync LeetCode submission Runtime - 1 ms (56.09%), Memory - 19.5 MB (17.01%)

--- solutions/0067-add-binary/solution.py
+++ solutions/0067-add-binary/solution.py
@@ -2,7 +2,4 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a=int(a,2)
         b=int(b,2)
-        aa=a+b
-        return bin(aa)[2:]
-        
-
+        return bin(a+b)[2:]
