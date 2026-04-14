aefbb100530aca39086a7e10ffaa96b50cc8b9d4
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-04-14 18:50:12 +0000

    Sync LeetCode submission Runtime - 11 ms (45.78%), Memory - 19.4 MB (6.49%)

---
+++ solutions/3421-count-pairs-that-form-a-complete-day-i/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        i=0
+        j=1
+        c=0
+        for i in range(len(hours)-1):
+            for j in range(i+1,len(hours)):
+                if(hours[i]+hours[j])%24==0:
+                    c+=1
+
+        return c
