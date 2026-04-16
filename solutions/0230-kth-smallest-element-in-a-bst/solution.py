74225fa4807548cdbac1558299469d03d1a95413
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-04-16 09:53:04 +0000

    Sync LeetCode submission Runtime - 4 ms (14.11%), Memory - 22.4 MB (10.81%)

---
+++ solutions/0230-kth-smallest-element-in-a-bst/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        arr=[]
+        def traverse(root):
+            if root:
+                traverse(root.left)
+                arr.append(root.val)
+                traverse(root.right)
+        traverse(root)
+        print(arr)
+        return(arr[k-1])
