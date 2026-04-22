58a3b2d75eb3f12e6ed136cc4fc1db4b2fe76e5e
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-04-22 12:24:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (88.25%)

---
+++ solutions/4275-traffic-signal-color/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def trafficSignal(self, timer: int) -> str:
+        if timer==0:
+            return "Green"
+        elif timer==30:
+            return "Orange"
+        elif 30<timer and timer<=90:
+            return "Red"
+        return "Invalid"
