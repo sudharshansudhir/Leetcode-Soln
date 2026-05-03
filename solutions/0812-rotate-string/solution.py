2516dbf94ce99f22fe3c77506a51bf6d4b8f713c
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-05-03 08:36:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (53.56%)

--- solutions/0812-rotate-string/solution.py
+++ solutions/0812-rotate-string/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
+        n=len(s)
         i=0
-        while i<len(s):
-            d=s[i:]+s[:i]
-            if(d==goal):
+        while i<n:
+            if s==goal:
                 return True
             i+=1
+            s=s[1:]+s[0]
         return False
