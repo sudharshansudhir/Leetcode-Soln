8a4273757cee6aa0d6c8d5f5daee73a7a0214f89
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-05-04 18:23:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (6.31%)

--- solutions/0048-rotate-image/solution.py
+++ solutions/0048-rotate-image/solution.py
@@ -3,17 +3,8 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # matrix[:] = list(zip(*matrix[::-1]))
-        news=[]
         for i in range(len(matrix)):
-            # t=[]
             for j in range(i,len(matrix)):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
             matrix[i].reverse()
-        
-            # for row in matrix:
-            #     row.reverse()
-        # print(news)
-        # matrix=news[:]
-
         
