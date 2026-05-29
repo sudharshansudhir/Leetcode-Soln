1edd2b85da8cd0f9c013a6b67ee9aed90b997b9a
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-05-29 09:02:33 +0000

    Sync LeetCode submission Runtime - 4 ms (34.73%), Memory - 23.9 MB (19.18%)

---
+++ solutions/0222-count-complete-tree-nodes/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        c=0
+        def traverse(node):
+            nonlocal c
+            if node:
+                c+=1
+                traverse(node.left)
+                traverse(node.right)
+        traverse(root)
+        return c
