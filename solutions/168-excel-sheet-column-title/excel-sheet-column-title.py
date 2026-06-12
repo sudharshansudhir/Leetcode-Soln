a9b94b596b87a8aa532ecea3e6c9d5f9ee40711d
Sudharshan.R <sudhir.ssr7@gmail.com>
2026-06-12 15:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.86%) - LeetSync

--- solutions/168-excel-sheet-column-title/excel-sheet-column-title.py
+++ solutions/168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -2,6 +2,9 @@ class Solution:
     def convertToTitle(self, n: int) -> str:
         ans=""
         while n:
-            n,q=divmod(n-1,26)
-            ans+=chr(q+ord("A"))
+            # n,q=divmod(n-1,26)
+            divi=(n-1)//26
+            modu=(n-1)%26
+            n=divi
+            ans+=chr(modu+ord("A"))
         return ans[::-1]
